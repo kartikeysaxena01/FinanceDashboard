7045836e532685e076873657d058be28fb88e00e
kartikeysaxena01 <kartikeysaxenaji4321@gmail.com>
2026-04-20 22:55:43 +0530

    Added swagger setup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
+
 const authRoutes = require("./routes/authRoutes");
 const recordRoutes = require("./routes/recordRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
@@ -10,6 +13,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ✅ Swagger here (IMPORTANT)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/test", (req, res) => {
+  res.send("API is working");
+});
 app.use("/auth", authRoutes);
 app.use("/records", recordRoutes);
 app.use("/dashboard", dashboardRoutes);
